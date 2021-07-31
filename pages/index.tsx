d968d1a9b6eb57fd2fc30cecd191cea448928943
ilyapopovs <ilya.popovs.dev@gmail.com>
2021-07-31 11:22:25 +0300

    use TS file extensions

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+    </div>
+  )
+}
