1ddad9cd4155188f05f136382e81c6591dfb659c
ilyapopovs <ilya.popovs.dev@gmail.com>
2021-08-12 00:36:03 +0300

    move btn classes to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-gray": {
+          DEFAULT: "#b5bdc4",
+          light: "#eef0f1",
+        }
+      },
+      fontFamily: {
+        noto: '"Noto Sans", sans-serif',
+      },
+    },
   },
   variants: {
     extend: {},
