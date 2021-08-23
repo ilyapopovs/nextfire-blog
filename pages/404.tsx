b05f24a6a97a01d5f7036a293d82db359e464b01
ilyapopovs <ilya.popovs.dev@gmail.com>
2021-08-23 22:46:14 +0300

    fix 404 on mobile

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,19 +2,30 @@ import Link from "next/link";
 
 export default function Custom404Page() {
   return (
-    <main className={"container py-4 flex justify-center"}>
+    <main className={"container flex justify-center"}>
       <div>
         <h1 className={"text-2xl"}>
           404 - That page does not seem to exist...
         </h1>
-        <iframe
-          src="https://giphy.com/embed/l2JehQ2GitHGdVG9y"
-          width="480"
-          height="362"
-          frameBorder="0"
-          allowFullScreen
-          className={"my-4"}
-        />
+        <div
+          className={"my-6"}
+          style={{
+            width: "100%",
+            height: "0",
+            paddingBottom: "75%",
+            position: "relative",
+          }}
+        >
+          <iframe
+            src="https://giphy.com/embed/l2JehQ2GitHGdVG9y"
+            width="100%"
+            height="100%"
+            style={{ position: "absolute" }}
+            frameBorder="0"
+            className="giphy-embed"
+            allowFullScreen
+          />
+        </div>
         <Link href="/">
           <button className={"btn btn-primary"}>Go home</button>
         </Link>
